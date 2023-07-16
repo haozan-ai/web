a94ce2a91bf72a6030fb5894cbff939ef4b8541f
sengmitnick <sengmitnick@163.com>
2023-07-16 08:08:34 +0800

    feat: add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
 import type { FC } from 'react'
 import React from 'react'
 
-import type { IMainProps } from '@/app/components'
 import Main from '@/app/components'
 
-const App: FC<IMainProps> = ({
-  params,
-}: any) => {
-  return (
-    <Main params={params} />
-  )
-}
+const App: FC = () => <Main />
 
 export default React.memo(App)
